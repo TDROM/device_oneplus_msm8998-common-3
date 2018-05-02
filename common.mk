03d68a2da0bef6a6f82dcef833e3ae61aecf2462
mukesh22584 <mks22584@gmail.com>
2018-05-02 11:07:58 +0530

    Add: prebuilt

--- common.mk
+++ common.mk
@@ -164,7 +164,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
-    Camera2
+    GoogleCamera \
+    OnePlusGallery
 
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0 \
